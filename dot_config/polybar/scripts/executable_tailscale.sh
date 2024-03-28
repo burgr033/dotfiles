6a1a32dcb7b59159f867d538bd86974964c6c3e2
sb <simon@burgr.io>
2024-03-28 10:28:35 +0100

    a

--- dot_config/polybar/scripts/executable_tailscale.sh
+++ dot_config/polybar/scripts/executable_tailscale.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 STOPPED="Tailscale is stopped."
+STRING=""
 if tailscale status | grep -q "$STOPPED"; then
-    echo "%{F#7F848E}󰚊%{F-}"
+    STRING="%{F#7F848E}󰚊%{F-}"
 else
-    echo "%{F#A3BE8C}󰚊%{F-}"
+    STRING="%{F#A3BE8C}󰚊%{F-}"
 fi
-
+VPN_NAME=$(nmcli --mode tabular --terse connection show --active | grep vpn | cut -d ':' -f1) 
+if [[ "${VPN_NAME}" != "" ]];
+then
+  STRING+=" "
+  STRING+="%{F#A3BE8C}%{F-}${VPN_NAME}"
+fi
+echo $STRING
